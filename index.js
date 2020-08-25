59795a853c7502b5e9de27d33bcc6399dce6274d
woxjro <n0i6c0k2@gmail.com>
2020-08-25 17:17:47 +0900

    twitter api get

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
+const TWITTER_API_CLIENT = require("./env");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+
+var Twitter = require("twitter");
+
+var client = new Twitter({
+  consumer_key: TWITTER_API_CLIENT.consumer_key,
+  consumer_secret: TWITTER_API_CLIENT.consumer_secret,
+  access_token_key: TWITTER_API_CLIENT.access_token_key,
+  access_token_secret: TWITTER_API_CLIENT.access_token_secret,
+});
+
+var params = { screen_name: "nodejs" };
+
+const getTweets = (params) => {
+  client.get("statuses/user_timeline", params, function (
+    error,
+    tweets,
+    response
+  ) {
+    if (error) throw error;
+    console.log(tweets);
+  });
+};
 
 app.use(
   bodyParser.urlencoded({
@@ -10,6 +33,7 @@ app.use(
 app.use(bodyParser.json());
 
 app.get("/", function (req, res) {
+  getTweets(params);
   res.send("Hello World!");
 });
 
